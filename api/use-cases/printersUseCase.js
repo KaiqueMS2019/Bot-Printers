48df105f25b7021ecac014fb3aa9891bf1a70639
Kaique Santos <kaiquema123@gmail.com>
2021-01-22 21:30:00 -0300

    Create Loading-bar

--- api/use-cases/printersUseCase.js
+++ api/use-cases/printersUseCase.js
@@ -1,5 +1,6 @@
 const PrinterRepository = require('../repositories/printerRepository')
 const PrinterService = require('../services/printerService')
+const fs = require('fs')
 
 
 const PrintersUseCase = {
@@ -21,9 +22,13 @@ const PrintersUseCase = {
         currentPrinter = parseInt(currentPrinter)
         const countPrinters = PrinterRepository.getAllPrinters().length
         const finish = (currentPrinter == countPrinters)
+        const tmpFiles = fs.readdirSync('screenshot/tmp')
+        const tmpFilesQuantity = tmpFiles.length
+        console.log(tmpFilesQuantity)
 
         return {
             totalPrinters: countPrinters,
+            nextPrinter: (currentPrinter+1),
             currentPrinter,
             finish
 
