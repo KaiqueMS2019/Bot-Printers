03ee9d33b11527293aba4fb7d2cfabfcd7a8ea26
Kaique Santos <kaiquema123@gmail.com>
2021-02-09 21:28:54 -0300

    Create CurrentPrinter and CurrentprinterState

--- front/src/components/shared/body/index.js
+++ front/src/components/shared/body/index.js
@@ -2,14 +2,42 @@ import React, { useEffect, useState } from 'react'
 import './body.css'
 import Printer from '../../../pages/index/printer'
 import IndexUseCase from '../../../use-cases/index-page/index.use-case'
+import axios from 'axios'
 
 const Body = () => {
 
     const indexUseCase = IndexUseCase
     const [printers, setPrinters] = useState([])
+    const [currentPrinterState, setCurrentPrinterState] = useState(1)
+
+    const recursivePrintDownload = async (currentPrinter) => {
+     return await axios.get(`http://localhost:3000/printers/all?currentPrinter=${currentPrinter}`)
+    }
+
+    const downloadAllPrinters = async (currentPrinter = 1) => {
+        console.log(currentPrinter)
+       try {
+        const response = await recursivePrintDownload(currentPrinter)
+        const responseData = response.data
+        const hasFinished = responseData.finish
+        
+        if(hasFinished){
+            alert("acabou")
+            return false
+        }
+
+        currentPrinter ++
+        setCurrentPrinterState(currentPrinter)
+        await downloadAllPrinters(currentPrinter)
+        console.log(response)
+       } catch (error) {
+           console.log(error)
+       } 
+    }
 
     useEffect(() => {
         if(!printers.length){
+            downloadAllPrinters()
             indexUseCase.loadAllPrinters()
             .then((payload) => {setPrinters(payload)})
             .catch((err) => setPrinters('Error'))
@@ -24,7 +52,7 @@ const Body = () => {
                     title="Baixar todos os relatórios">Baixar Todos</div>*/}
                      <div id="loading-bar-outside">
                         <div id="loading-bar-inside"></div>
-                        <div id="loading-bar-text">1/{printers.length}</div>
+                        <div id="loading-bar-text">{currentPrinterState}/{printers.length}</div>
                      </div>
                 </div> 
 
